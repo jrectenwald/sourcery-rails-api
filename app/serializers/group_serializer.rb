950fbcfc66f5462c13e607c88cc4e15a6d8bfbd1
medwards1771 <meredith.edwards1771@gmail.com>
2015-12-11 11:57:48 -0500

    fixes stack level too deep issue by writing attributes method on group serializer

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,5 +1,12 @@
 class GroupSerializer < ActiveModel::Serializer
   embed :ids, include: true
   attributes :id, :topic, :description
-  has_many :resources, :users
+
+  def attributes
+    attributes = super
+    attributes[:resources] = object.resources.try(:as_json)
+    attributes[:users] = object.users.try(:as_json)
+    attributes
+  end
+
 end
--- app/serializers/resource_serializer.rb
+++ app/serializers/resource_serializer.rb
@@ -2,4 +2,6 @@ class ResourceSerializer < ActiveModel::Serializer
   embed :ids, include: true
   attributes :id, :group_id, :user_id, :title, :link
   has_many :comments
+
+
 end
